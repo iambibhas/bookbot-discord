9fc263c5c094c59604d03b92f3702d1f41e37a31
Bibhas <github@bibhasdn.com>
2020-04-25 17:38:05 +0530

    added url request for query results

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const XML2JSON = require('xml2json');
 const client = new Discord.Client();
+const gr_key = process.env.GOODREADS_KEY;
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -8,9 +11,20 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.mentions.has(client.user)) {
         // we only care about messages where the bot is mentioned
+
+        // strip all the mentions from the message
         msg = message.content.replace(/<@!\d+>/gimu, "").trim()
-        if (msg === 'ping') {
-            message.channel.send('pong');
+
+        if (msg !== '') {
+            var gr_url = "http://www.goodreads.com/search/index.xml?key=" + gr_key + "&q=" + encodeURIComponent(msg);
+
+            fetch(gr_url)
+                .then(res => res.text())
+                .then(body => {
+                    console.log(body)
+                    var json_response = JSON.parse(XML2JSON.toJson(body));
+                    console.log(json_response);
+                });
         }
     }
 });
